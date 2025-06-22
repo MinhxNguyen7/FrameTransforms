9113c94f8f898b500e09d70b37a8c7376b978f21
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2025-06-21 17:40:40 -0700

    feat(example): explanations and intuition

--- example.py
+++ example.py
@@ -13,17 +13,21 @@ class Frame(Enum):
 
 
 def make_example_registry():
+    """
+    Creates an example XYZ registry of a robot at (0, 1, 0) with a camera one unit
+    above the base, rotated 90 degrees around the Y-axis.
+    """
     registry = Registry(Frame.WORLD)
 
     # Add transformations between frames
 
     world_to_base_transform = make_3d_transformation(
-        np.array([1, 0, 0]), Rotation.from_euler("xyz", [0, 0, 0], degrees=True)
+        np.array([0, 1, 0]), Rotation.from_euler("xyz", [0, 0, 0], degrees=True)
     )
     registry.add_transform(Frame.WORLD, Frame.BASE, world_to_base_transform)
 
     base_to_camera_transform = make_3d_transformation(
-        np.array([0, 1, 0]), Rotation.from_euler("xyz", [0, 90, 0], degrees=True)
+        np.array([0, 0, 1]), Rotation.from_euler("xyz", [0, 90, 0], degrees=True)
     )
     registry.add_transform(Frame.BASE, Frame.CAMERA, base_to_camera_transform)
 
@@ -31,6 +35,10 @@ def make_example_registry():
 
 
 def add_cycle_example():
+    """
+    Demonstrates adding a transformation that would create a cycle in the registry,
+    raising an InvaidTransformationError.
+    """
     registry = make_example_registry()
 
     # Attempt to add a transformation that creates a cycle
@@ -43,10 +51,13 @@ def add_cycle_example():
 
 
 def transitive_transformation_example():
+    """
+    Demonstrates getting a transformation from one frame to another through an intermediate frame.
+    """
     registry = make_example_registry()
 
     expected = make_3d_transformation(
-        np.array([1, 1, 0]), Rotation.from_euler("xyz", [0, 90, 0], degrees=True)
+        np.array([0, 1, 1]), Rotation.from_euler("xyz", [0, 90, 0], degrees=True)
     )
     actual = registry.get_transform(Frame.WORLD, Frame.CAMERA)
     assert np.allclose(actual[:3], expected[:3]), "Position mismatch"
