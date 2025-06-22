ca2ad5ae677d348b3f9f27014c5fcbc4116fa4d7
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2025-06-21 17:33:46 -0700

    refactor(example): extract exmple registry creation

--- example.py
+++ example.py
@@ -12,24 +12,30 @@ class Frame(Enum):
     CAMERA = "camera"
 
 
-if __name__ == "__main__":
-    register = Registry(Frame.WORLD)
+def make_example_registry():
+    registry = Registry(Frame.WORLD)
 
     # Add transformations between frames
 
     world_to_base_transform = make_3d_transformation(
         np.array([1, 0, 0]), Rotation.from_euler("xyz", [0, 0, 0], degrees=True)
     )
-    register.add_transform(Frame.WORLD, Frame.BASE, world_to_base_transform)
+    registry.add_transform(Frame.WORLD, Frame.BASE, world_to_base_transform)
 
     base_to_camera_transform = make_3d_transformation(
         np.array([0, 1, 0]), Rotation.from_euler("xyz", [0, 90, 0], degrees=True)
     )
-    register.add_transform(Frame.BASE, Frame.CAMERA, base_to_camera_transform)
+    registry.add_transform(Frame.BASE, Frame.CAMERA, base_to_camera_transform)
+
+    return registry
+
+
+if __name__ == "__main__":
+    registry = make_example_registry()
 
     # Attempt to add a transformation that creates a cycle
     try:
-        register.add_transform(Frame.CAMERA, Frame.WORLD, np.zeros(4))
+        registry.add_transform(Frame.CAMERA, Frame.WORLD, np.zeros(4))
     except InvaidTransformationError:
         print(
             "Caught invalid transformation because there is already a path between CAMERA and WORLD."
@@ -39,7 +45,7 @@ if __name__ == "__main__":
     expected = make_3d_transformation(
         np.array([1, 1, 0]), Rotation.from_euler("xyz", [0, 90, 0], degrees=True)
     )
-    actual = register.get_transform(Frame.WORLD, Frame.CAMERA)
+    actual = registry.get_transform(Frame.WORLD, Frame.CAMERA)
     assert np.allclose(actual[:3], expected[:3]), "Position mismatch"
     assert np.allclose(actual[3:], expected[3:]), "Rotation mismatch"
     print("Transformation from WORLD to CAMERA is correct.")
